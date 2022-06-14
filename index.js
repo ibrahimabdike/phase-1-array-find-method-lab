dde84adcf25067286e7748e8f384bf4aee3bd8df
Ibrahim Yahya <yahyalito1@gmail.com>
2022-06-14 10:38:40 +0300

    redone assignment
    redone assignment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // code your solution here
 superbowlWin = (record) => {
+
     const result = record.find( record => record.result === "W" );
+    // returns undefined if the record has no win objects
     return !!result ? result.year : undefined;
 }
